af34b6d80fbeb655262e384e30a350b3d798ba81
Awal Issah <awal.geek@gmail.com>
2024-06-24 14:29:11 +0000

    Initial commit of client setup

---
+++ nextcloud/nextcloud_data/config/config.php
@@ -0,0 +1,44 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocrgs2godw1d',
+  'passwordsalt' => 'f9Bfdf1sjaLJsxe4eAeASoJkN7zItU',
+  'secret' => 'n8Z/hla+L1P3btJEqZBJ4uaLnsiDYxgRofpuM8/qq5YpCI4K',
+  'trusted_domains' => 
+  array (
+    0 => 'drive.workspace.transformers.agency',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '27.1.1.0',
+  'overwrite.cli.url' => 'http://drive.workspace.transformers.agency',
+  'dbname' => 'nextcloud',
+  'dbhost' => '192.168.16.1',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => '!9VA*Ik*N2w!uc@9TLB',
+  'installed' => true,
+  'trusted_proxies'   => ['localhost:8088'],
+  'overwritehost'     => 'drive.workspace.transformers.agency',
+  'overwriteprotocol' => 'https',
+  'overwrite.cli.url' => 'https://drive.workspace.transformers.agency/',
+
+);
